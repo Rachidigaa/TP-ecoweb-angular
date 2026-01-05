5d0047ded0508c6801d567ca3b13c5e98a5607dc
Rachidigaa <rachid.iga@outlook.com>
2026-01-05 10:57:55 +0100

    BP18 - Favoriser les pages statiques

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  ngOnInit(): void {
+    // ❌ Mauvaise pratique : récupérer dynamiquement le contenu d'un footer statique
+    fetch('https://api.realworld.io/footer-content')
+      .then(res => res.json())
+      .then(data => {
+        console.log('Footer chargé dynamiquement inutilement', data);
+        // On pourrait ensuite mettre à jour le DOM si besoin, mais c'est complètement inutile
+      });
+
+    // Le footer est déjà statique dans le HTML, donc cette requête est un gaspillage
+    // ❌ Cela consomme CPU, réseau et ressources serveur inutilement
+  }
 
 }
